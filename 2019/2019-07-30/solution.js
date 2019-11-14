991620630b38ae160e85c0045a504a2df358a29f
Ian Harisay <imharisa@nuskin.com>
2019-11-13 19:53:02 -0700

    Fixed solution.  I misread the problem

--- 2019/2019-07-30/solution.js
+++ 2019/2019-07-30/solution.js
@@ -6,20 +6,34 @@
  * 
  * @param {Array<Integer>} arr 
  */
-const nextLowestPositiveInteger = (arr) => {
-    let lpi =  arr.reduce((acc,val) => {
-        console.log(`acc: ${acc}, val: ${val}`)
-        if(val > acc){
-            acc = val + 1
+const findLowestPositiveInteger = (arr) => {
+    let lpi = 1
+    arr.forEach(value => {
+        console.log(`lpi: ${lpi}, value: ${value}`)
+        if(value > 0 && value <= lpi){
+            lpi++
         }
-        return acc > 0 ? acc : 1
     })
 
     console.log(`given array: %j`,arr)
-    console.log(`next lowest positive integer is: ${lpi}`)
+    console.log(`The lowest positive integer is: ${lpi}`)
     console.log()
 }
 
-nextLowestPositiveInteger([3, 4, -1, 1])
-nextLowestPositiveInteger([-1, -5, -89])
-nextLowestPositiveInteger([1, 2, 0])
+findLowestPositiveInteger([3, 4, -1, 1])
+findLowestPositiveInteger([-1, -5, -89])
+findLowestPositiveInteger([1, 2, 0])
+
+const findLowestPositiveIntegerWithReduce = (acc,value) => {
+    if(acc < 0){
+        acc = 1
+    }
+    
+    if(value > 0 && value <= acc){
+        acc++
+    }
+    return acc
+}
+
+let answer = [-1, -5, -89].reduce(findLowestPositiveIntegerWithReduce)
+console.log(`answer: ${answer}`)
